8283b8c9733afae72252df866eed29f245289df5
web studio ivvy.ru london <go@ivvy.ru>
2017-04-14 03:11:32 +0400

    registration.php theme.xml

---
+++ app/design/frontend/Goivvy/rwd/registration.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * It is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to sales@goivvy.com so we can send you a copy immediately.
+ *
+ * @component  Goivvy_Rwd
+ * @copyright  Copyright (c) 2017 Goivvy.com. (https://www.goivvy.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author     Goivvy.com <team@goivvy.com>
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Goivvy/rwd',
+    __DIR__
+);
